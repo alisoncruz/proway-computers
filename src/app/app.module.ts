653da2d5fb5ecda61aef254b8a617e245256af6e
alisoncruz <alisonn83@gmail.com>
2022-10-17 17:26:12 -0300

    Add search bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NaoEncontradoComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
